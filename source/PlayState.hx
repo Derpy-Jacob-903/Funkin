a33298dbf3ff9f8ccaad093a7eeee0d54ca897be
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2021-08-25 09:03:39 -0600

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2312,14 +2312,18 @@ class PlayState extends MusicBeatState
 		if (health > 2)
 			health = 2;
 		if (healthBar.percent < 20)
+		{
 			iconP1.animation.curAnim.curFrame = 1;
 		else
 			iconP1.animation.curAnim.curFrame = 0;
+		}
 
 		if (healthBar.percent > 80)
+		{
 			iconP2.animation.curAnim.curFrame = 1;
 		else
 			iconP2.animation.curAnim.curFrame = 0;
+		}
 
 		/* if (FlxG.keys.justPressed.NINE)
 			FlxG.switchState(new Charting()); */
